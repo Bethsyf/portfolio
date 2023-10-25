8281f8c0809e88c3079a6d4942685d58883d0d29
Bethsyf <bethsyfalcon@gmail.com>
2023-10-25 07:42:04 -0500

    Refactor: fix footer

--- src/components/views/footer/Footer.tsx
+++ src/components/views/footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './Footer.module.scss';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { SiGmail } from 'react-icons/si';
 
 const Footer = () => {
   return (
@@ -7,27 +9,30 @@ const Footer = () => {
       <div className={s.container} id="contact">
         <div className={s.info}>
           <h2>Contacto</h2>
-          <p>Contacto: bethsyfalcon@gmail.com</p>
-          <p>
-            LinkedIn:{' '}
+          <div className={s.rrss}>
+            <SiGmail />
+            <p>bethsyfalcon@gmail.com</p>
+          </div>
+          <div className={s.rrss}>
+            <FaLinkedinIn />
             <a
               href="https://www.linkedin.com/in/tu-linkedin"
               target="_blank"
               rel="noopener noreferrer"
             >
-              @TuLinkedIn
+              @Bethsy Falcon
             </a>
-          </p>
-          <p>
-            GitHub:{' '}
+          </div>
+          <div className={s.rrss}>
+            <FaGithub />
             <a
-              href="https://github.com/tu-usuario"
+              href="https://github.com/Bethsyf"
               target="_blank"
               rel="noopener noreferrer"
             >
-              @TuUsuario
+              @Bethsyf
             </a>
-          </p>
+          </div>
         </div>
         <p className={s.pd}>Made with ❤️ by @Bethsyf</p>
       </div>
