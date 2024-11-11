f6d605ee2129985205a4a8793d49e262b2a6b878
Bethsyf <bethsyfalcon@gmail.com>
2024-11-11 16:40:29 -0500

    fix href experience

--- src/components/views/navbar/NavbarView.tsx
+++ src/components/views/navbar/NavbarView.tsx
@@ -68,7 +68,7 @@ const NavbarView = () => {
               </a>
             </li>
             <li className={s.item}>
-              <a className={s.link} href="#projects">
+              <a className={s.link} href="#experience">
               Experiencia
               </a>
             </li>
