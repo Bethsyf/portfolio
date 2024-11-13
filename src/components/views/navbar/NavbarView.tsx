f80289943553ec313baa988d6020dc7410d1021e
Bethsyf <bethsyfalcon@gmail.com>
2024-11-13 07:24:45 -0500

    fix error download cv

--- src/components/views/navbar/NavbarView.tsx
+++ src/components/views/navbar/NavbarView.tsx
@@ -92,7 +92,7 @@ const NavbarView = () => {
             <li className={s.item}>
             <a
                 className={`${s.link} ${s.downloadButton}`}
-                href="../../../../public/cv.pdf"
+                href="/cv.pdf"
                 download="CV_Bethsy_Falcon"
               >
                 <FiDownload className={s.downloadIcon} /> Descargar CV
