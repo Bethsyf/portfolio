eea73d63672d1f798e8d0bbaaa9001197eb913d7
Bethsyf <bethsyfalcon@gmail.com>
2024-11-18 18:36:09 -0500

    feat: internationalitation for navbar

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'ejemplo.com'],
   },
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    localeDetection: true,
+  },
 };
 
 module.exports = nextConfig;
