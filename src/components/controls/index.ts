19460db2cd432f334fd00039100c517bf7251b33
Bethsy Falcon <bethsyfalcon@gmail.com>
2026-03-17 12:25:39 -0500

    refactor(controls): add barrel file for cleaner imports

---
+++ src/components/controls/index.ts
@@ -0,0 +1,5 @@
+export { default as ButtonControl } from "./button/ButtonControl";
+export { default as CardControl } from "./card/CardControl";
+export { default as LanguageToggleControl } from "./languageToggle/LanguagetoggleControl";
+export { default as TextControl } from "./text/TextControl";
+export { default as ThemeToggleControl } from "./themeToggle/ThemeToggleControl";
